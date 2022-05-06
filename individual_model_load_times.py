e672210a6dbe3e7d7b301c3416729018b248db6a
Lucian Smith <lpsmith@uw.edu>
2022-05-06 13:42:54 -0700

    Initial versions of scripts.

---
+++ individual_model_load_times.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  4 12:22:47 2022
+
+@author: Lucian
+"""
+
+# import tellurium as te
+import roadrunner
+from os import walk
+import time
+import numpy as np
+
+def writeTimesFor(sbmlfile, timedata, times):
+    averageLL = np.average(timedata["LLJit"][sbmlfile])
+    averageMC = np.average(timedata["MCJit"][sbmlfile])
+    stdLL = np.std(timedata["LLJit"][sbmlfile])
+    stdMC = np.std(timedata["MCJit"][sbmlfile])
+    ratio = averageMC/averageLL
+    times.write(", ")
+    times.write(str(averageLL))
+    times.write(", ")
+    times.write(str(stdLL))
+    times.write(", ")
+    times.write(str(averageMC))
+    times.write(", ")
+    times.write(str(stdMC))
+    times.write(", ")
+    times.write(str(ratio))
+
+
+biomds = "C:/Users/Lucian/Desktop/temp-biomodels/final"
+delays = ["BIOMD0000000024", "BIOMD0000000025", "BIOMD0000000034", "BIOMD0000000154", "BIOMD0000000155", "BIOMD0000000196", "BIOMD0000000841"]
+
+bmfiles = []
+for root, __, files in walk(biomds):
+    include = True
+    for delay in delays:
+        if delay in root:
+            include = False
+    if include:
+        bmfiles.append((root, files))
+    
+sbmlfiles = []
+
+for (root, files) in bmfiles:
+    
+    for file in files:
+        if ".xml" not in file:
+            continue
+        sbmlfiles.append(root + "/" + file)
+
+loadtime = {}
+loadtime["LLJit"] = {}
+loadtime["MCJit"] = {}
+for sbmlfile in sbmlfiles:
+    loadtime["LLJit"][sbmlfile] = []
+    loadtime["MCJit"][sbmlfile] = []
+
+simtime = {}
+simtime["LLJit"] = {}
+simtime["MCJit"] = {}
+for sbmlfile in sbmlfiles:
+    simtime["LLJit"][sbmlfile] = []
+    simtime["MCJit"][sbmlfile] = []
+
+for n in range(5):
+    print("Repeat", n)
+    for sbmlfile in sbmlfiles:
+        # print(sbmlfile)
+        for (backend, bstr) in [(roadrunner.Config.LLJIT, "LLJit"), (roadrunner.Config.MCJIT, "MCJit")]:
+            roadrunner.Config.setValue(roadrunner.Config.LLVM_BACKEND, backend)
+            try:
+                pre = time.perf_counter()
+                r = roadrunner.RoadRunner(sbmlfile)
+                post_load = time.perf_counter()
+                r.simulate(0, 100, 1000)
+                post_sim = time.perf_counter()
+                loadtime[bstr][sbmlfile].append(post_load - pre)
+                simtime[bstr][sbmlfile].append(post_sim - post_load)
+            except Exception as e:
+                print(sbmlfile)
+                print(e)
+
+times = open("individual_times.csv", "w")
+times.write("File")
+times.write(", ")
+times.write("LLJit load")
+times.write(", ")
+times.write("err LLJit load")
+times.write(", ")
+times.write("MCJit load")
+times.write(", ")
+times.write("err MCJit load")
+times.write(", ")
+times.write("MC/LL load")
+times.write(", ")
+times.write("LLJit sim")
+times.write(", ")
+times.write("err LLJit sim")
+times.write(", ")
+times.write("MCJit sim")
+times.write(", ")
+times.write("err MCJit sim")
+times.write(", ")
+times.write("MC/LL sim")
+times.write("\n")
+averages = {}
+stdevs = {}
+totalsLL = np.zeros(10)
+totalsMC = np.zeros(10)
+for sbmlfile in loadtime[bstr]:
+    times.write(sbmlfile)
+    
+    writeTimesFor(sbmlfile, loadtime, times)
+    writeTimesFor(sbmlfile, simtime, times)
+
+    times.write("\n")
+times.close()
+
+print(totalsLL)
+print(totalsMC)
+print("Average LL:", np.average(totalsLL))
+print("Average MC:", np.average(totalsMC))
+print("Std LL:", np.std(totalsLL))
+print("Std MC:", np.std(totalsMC))
+     
+      
---
+++ load_or_load_and_simulate_fig1_fig2.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  4 12:22:47 2022
+
+@author: Lucian
+"""
+
+import roadrunner
+from os import walk
+import time
+import concurrent
+import numpy as np
+
+
+def getSBMLFilesFromBiomodels(biomds = "C:/Users/Lucian/Desktop/temp-biomodels/final"):
+    delays = ["BIOMD0000000024", "BIOMD0000000025", "BIOMD0000000034", "BIOMD0000000154", "BIOMD0000000155", "BIOMD0000000196", "BIOMD0000000841"]
+    bmfiles = []
+    for root, __, files in walk(biomds):
+        include = True
+        for delay in delays:
+            if delay in root:
+                include = False
+        if include:
+            bmfiles.append((root, files))
+        
+    sbmlfiles = []
+    
+    for (root, files) in bmfiles:
+        
+        for file in files:
+            if ".xml" not in file:
+                continue
+            sbmlfiles.append(root + "/" + file)
+    
+    #If we only want a few files for testing.
+    #sbmlfiles = sbmlfiles[:10]
+    return sbmlfiles
+    
+    # timing = {}
+    # for sbmlfile in sbmlfiles:
+    #     timing[sbmlfile] = []"w")
+    
+def loadOnly(sbmlfile):
+    roadrunner.RoadRunner(sbmlfile)
+    
+def loadAndSimulate(sbmlfile):
+    r = roadrunner.RoadRunner(sbmlfile)
+    r.simulate(0, 10, 100)
+    
+def runExperiment(sbmlfiles, nrepeats, ncores, function, allcores):
+    timevecs = {}
+    timevecs["LLJit"] = {}
+    timevecs["MCJit"] = {}
+    if allcores:
+        threadrange = range(ncores)
+    else:
+        threadrange = [0, 1, ncores-1]
+    for nthread in threadrange:
+        timevecs["LLJit"][nthread] = []
+        timevecs["MCJit"][nthread] = []
+    for repeat in range(nrepeats):
+        for nthreads in threadrange:
+            for (backend, bstr) in [(roadrunner.Config.LLJIT, "LLJit"), (roadrunner.Config.MCJIT, "MCJit")]:
+                roadrunner.Config.setValue(roadrunner.Config.LLVM_BACKEND, backend)
+                if nthreads > 0:
+                    print("Starting run with", nthreads, "threads, repeat", repeat+1, "backend", bstr)
+                    start = time.perf_counter()
+                    with concurrent.futures.ProcessPoolExecutor(max_workers=nthreads) as executor:
+                        executor.map(function, sbmlfiles)
+                else:
+                    print("Starting non-parallel run.  Repeat", repeat+1, "backend", bstr)
+                    start = time.perf_counter()
+                    for sbmlfile in sbmlfiles:
+                        loadOnly(sbmlfile)
+                    
+                end = time.perf_counter()
+                timevecs[bstr][nthreads].append(end-start)
+                print("Total time:", end-start)
+    return timevecs, threadrange
+
+def saveTimeVecs(timevecs, threadrange, filename):
+    out = open(filename, "w")
+    out.write("Ntheads")
+    out.write(",")
+    out.write("LLJit")
+    out.write(",")
+    out.write("MCJit")
+    out.write(",")
+    out.write("err LLJit")
+    out.write(",")
+    out.write("err MCJit")
+    out.write("\n")
+    
+    for nthread in threadrange:
+        LLaverage = np.average(timevecs["LLJit"][nthread])
+        LLstd = np.std(timevecs["LLJit"][nthread])
+        MCaverage = np.average(timevecs["MCJit"][nthread])
+        MCstd = np.std(timevecs["MCJit"][nthread])
+        ratio = MCaverage / LLaverage
+        
+        out.write(str(nthread))
+        out.write(",")
+        out.write(str(LLaverage))
+        out.write(",")
+        out.write(str(MCaverage))
+        out.write(",")
+        out.write(str(LLstd))
+        out.write(",")
+        out.write(str(MCstd))
+        out.write("\n")
+    out.close()
+    
+if __name__ == '__main__':
+    sbmlfiles = getSBMLFilesFromBiomodels()
+    (timevecs, threadrange) = runExperiment(sbmlfiles, 10, 3, loadOnly, True)
+    saveTimeVecs(timevecs, threadrange, "fig1_only_load.csv")
+    (timevecs, threadrange) = runExperiment(sbmlfiles, 10, 3, loadAndSimulate, False)
+    saveTimeVecs(timevecs, threadrange, "fig2_load_and_sim.csv")
